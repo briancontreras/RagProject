e18c7f76c18e8811984ce20b0472ee9d6c354103
briancontreras <bcontr41@calstatela.edu>
2025-07-10 15:22:09 -0700

    added more urls and added a method to scrape the web urls instead of an independent for loop

--- src/webScrape.py
+++ src/webScrape.py
@@ -23,19 +23,19 @@ def writeToSingleFileWestLaw(soup, fileName):
     with open(fileName , "a") as file:
         file.write("\n")
 
-
+def scrapeUrls(urls, textFileName):
+    for url in westLawUrls: 
+        html_document = getHTMLdocument(url)
+        forSoup = BeautifulSoup(html_document, 'html.parser')
+        writeToSingleFileWestLaw(forSoup, 'test.txt')
 
 westLawUrls = [
     "https://govt.westlaw.com/calregs/Document/I7A6B47D0FD4311ECBA0CE8BD2C3F45C2?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)",
     "https://govt.westlaw.com/calregs/Document/ICF14695063E711EDB5569A0BCCCD916B?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)",
     "https://govt.westlaw.com/calregs/Document/I7F6CE1A34C6611EC93A8000D3A7C4BC3?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)",
+    "https://govt.westlaw.com/calregs/Document/I406861A35A0D11EC8227000D3A7C4BC3?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)",
+    "https://govt.westlaw.com/calregs/Document/I408F98B35A0D11EC8227000D3A7C4BC3?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)",
+    "https://govt.westlaw.com/calregs/Document/I40B3C2835A0D11EC8227000D3A7C4BC3?viewType=FullText&originationContext=documenttoc&transitionType=CategoryPageItem&contextData=(sc.Default)"
     ]
-url_to_scrape = westLawUrls[0]
-html_document = getHTMLdocument(url_to_scrape)
-testSOUP = BeautifulSoup(html_document,'html.parser')
 
-for url in westLawUrls: 
-    html_document = getHTMLdocument(url)
-    forSoup = BeautifulSoup(html_document, 'html.parser')
-    writeToSingleFileWestLaw(forSoup, 'test.txt')
-# writeToSingleFileWestLaw(testSOUP,'test.txt')
+scrapeUrls(westLawUrls, 'test.txt')
