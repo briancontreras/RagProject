22ce367cb5b784421a45576ba2e93332a0d96dfa
briancontreras <bcontr41@calstatela.edu>
2025-07-12 21:06:39 -0700

    added make request method in api class

--- src/LegiScanAPI.py
+++ src/LegiScanAPI.py
@@ -1,5 +1,7 @@
 #allows for us to make API calls
 import requests
+#Needs Json class to utilize json response data
+import json
 
 #classes to load env api key
 import os
@@ -20,4 +22,25 @@ class LegiScanAPI:
 
         #sets session to not have to make multiple requests
         self.session = requests.Session()
+    def _make_request(self, endpoint: str, params: Dict = None) -> Dict:
+        if params is None:
+            params = {}
+        params['key'] = self.api_key
+
+        try:
+            response = self.session.get(f"{self.base_url}{endpoint}", params=params)
+            response.raise_for_status
+
+            data = response.json
+            
+            if data.get('status') == 'ERROR':
+                raise Exception(f"API ERROR: {data.get('alert', 'Unknown error')}")
+
+            return data
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed {e}")
+            raise
+        except json.JSONDecodeError as e:
+            print(f"Failed to parse JSON response: {e}")
+            raise
 
