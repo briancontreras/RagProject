1a349dfc397430ca26b729c8f2fb42596df6dce4
briancontreras <bcontr41@calstatela.edu>
2025-07-17 15:37:55 -0700

    checking more zip file input things

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 from LegiScanAPI import LegiScanAPI
 import zipProcessor
 import os
+import base64
+
 
 
 api = LegiScanAPI(  )
@@ -13,7 +15,12 @@ access_key = jsonFile[0]['access_key']
 print(id)
 print(access_key)
 dataset = api.get_dataset(id,access_key,"json")
-zip_file = zipProcessor.extract_all_from_base64_zip(dataset , "./extracted_data")
+dataset = base64.decode(dataset)
+
+with open("zipCache", 'a',encoding="utf-8") as file:
+            file.write(dataset)
+
+# zip_file = zipProcessor.extract_all_from_base64_zip(dataset , "./extracted_data")
 
 
 
